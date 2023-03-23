073de45361ecc7c80a640358d2d3d9da8979e1e7
sanjithsarkar <sanjithsarkar2@gmail.com>
2023-03-23 22:54:16 +0600

    completed Team section with using owl-carousel and waypoint plugin

--- js/script.js
+++ js/script.js
@@ -1,4 +1,55 @@
-$(window).on('load', function(){
+$(window).on('load', function () {
     $('#status').fadeOut();
     $('#preloader').delay(550).fadeOut();
 });
+
+// ========================================
+//               Team Member
+//=========================================
+
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        items: 2,
+        autoplay: true,
+        smartSpeed: 50,
+        loop: true,
+        autoplayHoverPause: true,
+        nav: true,
+        dots: false,
+        navText: ['<i class="fa-solid fa-angle-left"></i>', '<i class="fa-solid fa-angle-right"></i>']
+    });
+});
+
+
+//   =====================================
+//                Progress Bars
+//   =====================================
+
+$(".progressElement").waypoint(function() {
+
+    // code to execute when the waypoint is triggered
+    $(".progress-bar").each(function () {
+        $(this).animate({
+            width: $(this).attr("aria-valuenow") + "%"
+        }, 1000);
+    });
+    this.destroy();
+  }, { offset: 'bottom-in-view' });
+  
+  
+
+
+// $(document).ready(function () {
+//     $(".progress-bar").each(function () {
+//         $(this).animate({
+//             width: $(this).attr("aria-valuenow") + "%"
+//         }, 1500)
+//     });
+
+// })
+
+
+
+
+
+
