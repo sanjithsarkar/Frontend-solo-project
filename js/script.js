b294a39202bfb6e0d18d478fcac5345225050c46
sanjithsarkar <sanjithsarkar2@gmail.com>
2023-03-30 22:08:21 +0600

    completed

--- js/script.js
+++ js/script.js
@@ -1,3 +1,52 @@
+
+/*========================================================================
+                    Show and Hide Navigation Color
+==========================================================================*/
+$(function () {
+
+    // show/hide nav on page load
+    showHideNav();
+
+    $(window).scroll(function () {
+
+        // show/hide nav on window's scroll
+        showHideNav();
+
+    });
+
+    function showHideNav() {
+
+            
+        if ($(window).scrollTop() > 100) {
+
+            // Show White nav
+            $("nav").addClass("bg-secondary");
+
+            // Show Dark logo
+            $(".navbar-brand img").attr("src", "image/logo/logo-dark.png");
+
+            // Show back to top button
+            // $("#back-to-top").fadeIn();
+        } else {
+
+            // Hide White nav
+            $("nav").removeClass("bg-secondary");
+
+            // Show logo
+            $(".navbar-brand img").attr("src", "image/logo/logo.png");
+
+            // Hide back to top button
+            // $("#back-to-top").fadeOut();
+        }
+    }
+});
+
+
+
+/*=========================================
+               Preloader
+========================================*/
+
 $(window).on('load', function () {
     $('#status').fadeOut();
     $('#preloader').delay(550).fadeOut();
@@ -25,7 +74,7 @@ $(document).ready(function () {
 //                Progress Bars
 //   =====================================
 
-$(".progressElement").waypoint(function() {
+$(".progressElement").waypoint(function () {
 
     // code to execute when the waypoint is triggered
     $(".progress-bar").each(function () {
@@ -34,19 +83,19 @@ $(".progressElement").waypoint(function() {
         }, 1000);
     });
     this.destroy();
-  }, { offset: 'bottom-in-view' });
-  
-  
+}, { offset: 'bottom-in-view' });
 
 
-// $(document).ready(function () {
-//     $(".progress-bar").each(function () {
-//         $(this).animate({
-//             width: $(this).attr("aria-valuenow") + "%"
-//         }, 1500)
-//     });
 
-// })
+
+$(document).ready(function () {
+    $(".progress-bar").each(function () {
+        $(this).animate({
+            width: $(this).attr("aria-valuenow") + "%"
+        }, 1500)
+    });
+
+})
 
 
 // ===========================================
@@ -58,7 +107,58 @@ $('#service-tabs').responsiveTabs({
 });
 
 
+/*=========================================
+             Smooth Scrolling
+========================================*/
+
+$(function(){
+
+    $(".smooth-scroll").click(function(event){
+
+        event.preventDefault();
+
+        // get section id like #home, #about, #team, #service and etc
+
+        var section_id = $(this).attr("href");
+
+        $("html, body").animate({
+            scrollTop: $(section_id).offset().top 
+        }, 1000);
+        
+    });
+});
+
+
+/*=========================================
+             Back to Top Arrow
+========================================*/
 
 
 
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 50 ) {
+        $('#back-to-top').fadeIn();
+    } else {
+        $('#back-to-top').fadeOut();
+    }
+});
 
+
+$("#back-to-top").click(function() {
+    $("html, body").animate({scrollTop: 0}, 1000);
+ });
+
+
+/*========================================================
+                    Animate on scroll
+==========================================================*/
+
+new WOW().init();
+
+$(window).on('load', function(){
+    $("#home-heading-1").addClass("animated fadeInDown");
+    $("#home-heading-2").addClass("animated fadeInLeft");
+    $("#home-text").addClass("animated zoomIn");
+    $("#home-btn").addClass("animated zoomIn");
+    $("#arrow-down i").addClass("animated fadeInDown infinite");
+})
